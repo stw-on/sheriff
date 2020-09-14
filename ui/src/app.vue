1dd08a29cf2accbf76ea2244ec4e91868e49501e
Timo Schwarzer <me@timoschwarzer.com>
2020-09-14 16:55:18 +0200

    Polish index page

--- ui/src/app.vue
+++ ui/src/app.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition>
     </v-main>
   </v-app>
 </template>
