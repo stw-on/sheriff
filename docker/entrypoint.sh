e9aa321beb51727d2a19fda057662527bba30a68
Timo Schwarzer <me@timoschwarzer.com>
2020-09-14 15:13:29 +0200

    Fix sh syntax in production entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,7 +13,7 @@ exec_as_www_data() {
 if [ "$1" = "worker" ]; then
   wait-for-it app:80 -t 3600
   exec_as_www_data php artisan queue:work --tries=5 --delay=30
-elif [[ "$1" == "scheduler" ]]; then
+elif [ "$1" = "scheduler" ]; then
   wait-for-it app:80 -t 3600 || exit 1
   exec_as_www_data php artisan cron -q
 else
