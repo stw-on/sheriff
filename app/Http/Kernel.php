591fc1b4a3b39a860afa6566bb3f6c7b11713a65
Timo Schwarzer <timo@schwarzer.dev>
2021-10-27 12:04:30 +0200

    Disable API rate limiting

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
